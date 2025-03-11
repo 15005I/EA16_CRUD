32226dbbffca11af22966e744fa740f68c2d4480
15005I <15005@portalcedi.edu.mx>
2025-03-11 10:41:12 -0600

    EA16

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonButton, IonCheckbox, IonInput } from '@ionic/angular/standalone';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ReactiveFormsModule, IonItem, IonLabel, IonButton, IonCheckbox, IonInput, CommonModule],
 })
 export class HomePage {
+  tareas: { nombre: string | null; fecha: string | null; descripcion: string | null; completada: boolean }[] = [];
+
+  tarea = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    fecha: new FormControl('', Validators.required),
+    descripcion: new FormControl('')
+  });
+
+  nTarea() {
+    if (this.tarea.valid) {
+      const nuevaTarea = {
+        nombre: this.tarea.value.nombre!,
+        fecha: this.tarea.value.fecha!,
+        descripcion: this.tarea.value.descripcion!,
+        completada: false
+      };
+      this.tareas.push(nuevaTarea);
+
+      this.tarea.reset();
+    }
+  }
   constructor() {}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBaZ_8O-70iZZWC7nkHaxrO69-Ig4_DJYw",
+    authDomain: "crud-53063.firebaseapp.com",
+    projectId: "crud-53063",
+    storageBucket: "crud-53063.firebasestorage.app",
+    messagingSenderId: "1016082888784",
+    appId: "1:1016082888784:web:af9614759072e0dfe23955",
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
